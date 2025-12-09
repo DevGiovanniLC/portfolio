2e20c2651a78d7330a84ed053760f7401ed208a2
Giovanni León <giovannilc.dev@gmail.com>
2025-12-09 11:46:21 +0000

    ✨ feat(optimización imagen):
    Agrega lazy loading a todas las imagenes y usa el componente Image de Atro que optimiza la imagen

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    vite: {
+        // @ts-ignore
+        plugins: [tailwindcss()],
+    },
+    image: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'cdn.jsdelivr.net',
+            },
+        ],
+    },
 });
